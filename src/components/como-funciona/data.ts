7ac69c48b45450cb734f2241c1926b596045c997
Olga Makeikina <olgamakeikina@gmail.com>
2025-08-28 13:33:18 -0300

    процесс, надо доделать

--- src/components/como-funciona/data.ts
+++ src/components/como-funciona/data.ts
@@ -9,24 +9,7 @@ export const processSteps: ProcessStep[] = [
     seguranca: "Agende seu horário",
     imagem: "/images/process/0_3 (16).jpeg"
   },
-  {
-    numero: "2", 
-    titulo: "Receba a confirmação e o link",
-    descricao: "O link chega no WhatsApp e no e-mail",
-    detalhes: "Por ele você poderá escolher o horário mais conveniente online",
-    seguranca: "Confirmação instantânea e agendamento garantido",
-    imagem: "/images/process/0_0 (18).jpeg"
-  },
-  {
-    numero: "3",
-    titulo: "Compareça e pague com desconto",
-    descricao: "Pagamento após o manicure",
-    detalhes: "Aproveite o serviço VIP e pague no local com vantagem",
-    seguranca: "Garantia de 100% de satisfação",
-    imagem: "/images/process/0_0 (7).jpeg"
-  }
-]
-  /*  {
+ {
     numero: "2", 
     titulo: "Pague a entrada",
     descricao: "PIX ou cartão - segurança total",
@@ -34,4 +17,20 @@ export const processSteps: ProcessStep[] = [
     seguranca: "Pagamento da entrada é 100% seguro. Se não puder comparecer, reagendamos ou devolvemos o valor",
     imagem: "/images/process/0_0 (8) (1).jpeg"
   },
-  */
+  {
+    numero: "3", 
+    titulo: "Receba a confirmação e o link",
+    descricao: "O link chega no WhatsApp e no e-mail",
+    detalhes: "Por ele você poderá escolher o horário mais conveniente online",
+    seguranca: "Confirmação instantânea e agendamento garantido",
+    imagem: "/images/process/0_0 (18).jpeg"
+  },
+  {
+    numero: "4",
+    titulo: "Compareça e pague com desconto",
+    descricao: "Pagamento após o manicure",
+    detalhes: "Aproveite o serviço VIP e pague no local com vantagem",
+    seguranca: "Garantia de 100% de satisfação",
+    imagem: "/images/process/0_0 (7).jpeg"
+  }
+]
