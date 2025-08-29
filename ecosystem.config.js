0871c4b2d3f896186b422693ee2efe53bb80bbb1
Olga Makeikina <olgamakeikina@gmail.com>
2025-08-29 10:40:55 -0300

    feat: optimize PM2 config for Next.js standalone mode

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,8 @@ module.exports = {
   apps: [
     {
       name: 'gomanic-landing-br',
-      script: 'npm',
-      args: 'start',
+      script: 'node',
+      args: '.next/standalone/server.js',
       cwd: '/home/zardes/gomanic-landing-br',
       instances: 1,
       autorestart: true,
@@ -11,12 +11,14 @@ module.exports = {
       max_memory_restart: '1G',
       env: {
         NODE_ENV: 'production',
-        PORT: 3000
+        PORT: 3000,
+        HOSTNAME: '0.0.0.0'
       },
-      error_file: '/home/zardes/gomanic-landing-br/logs/err.log',
-      out_file: '/home/zardes/gomanic-landing-br/logs/out.log',
       log_file: '/home/zardes/gomanic-landing-br/logs/combined.log',
+      out_file: '/home/zardes/gomanic-landing-br/logs/out.log',
+      error_file: '/home/zardes/gomanic-landing-br/logs/error.log',
       time: true
     }
   ]
 };
+
